a9beb552eaf37419bb46f54e27d2b1c4389d74c6
Peenchayakorn Tanakornpornsawas <148639119+peenchayadev@users.noreply.github.com>
2024-08-10 19:10:37 +0700

    c

--- src/app/[locale]/(unauth)/layout.tsx
+++ src/app/[locale]/(unauth)/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { ReactNode } from 'react'
 
 export default function RootLayout({ children }: { children: ReactNode }) {
@@ -5,10 +6,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   //   RENDER
   //---------------------
   return (
-    <html>
-      <body>
-        {children}
-      </body>
+    <html lang="en">
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
+      </Head>
+      <body>{children}</body>
     </html>
   )
 }
--- src/components/Home/LoginButton.tsx
+++ src/components/Home/LoginButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import classNames from 'classnames';
+import classNames from 'classnames'
 
 interface LoginButtonProps {
   icon?: string
@@ -10,7 +10,7 @@ export const LoginButton = (props: LoginButtonProps) => {
   return (
     <div className="bg-white rounded-2xl px-[5px] py-[5px] border border-slate-400 w-fit h-fit">
       <div className="flex gap-[5px] justify-center items-center">
-      <i className="fa-solid fa-bars"></i>
+      <FontAwesomeIcon icon="fa-solid fa-bars" />
         <div className="bg-white rounded-full w-[32px] h-[32px] border border-slate-400"></div>
       </div>
     </div>
